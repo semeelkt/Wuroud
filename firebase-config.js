a4cf730733a83c7d3d480ebd5f9de904e23e4ccf
mohammed semeel kt <mrflux3602@gmail.com>
2025-09-24 12:32:04 +0000

    edited

--- firebase-config.js
+++ firebase-config.js
@@ -3,7 +3,7 @@ export const firebaseConfig = {
   apiKey: "AIzaSyAbCtlIKuybglpbCmUfzbo6oy5kS0Aiits",
   authDomain: "wuroud-8fa99.firebaseapp.com",
   projectId: "wuroud-8fa99",
-  storageBucket: "wuroud-8fa99.firebasestorage.app",
+  storageBucket: "wuroud-8fa99.appspot.com",
   messagingSenderId: "1056456782887",
   appId: "1:1056456782887:web:7298bdbe27031fdeeb4a02"
 };
