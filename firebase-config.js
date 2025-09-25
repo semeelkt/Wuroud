b90cd297590748fe104703bdf3402bf2ca77493e
mohammed semeel kt <mrflux3602@gmail.com>
2025-09-25 00:39:41 +0000

    edited

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,9 @@
 // Your web app's Firebase configuration
-export const firebaseConfig = {
+const firebaseConfig = {
   apiKey: "AIzaSyAbCtlIKuybglpbCmUfzbo6oy5kS0Aiits",
   authDomain: "wuroud-8fa99.firebaseapp.com",
   projectId: "wuroud-8fa99",
-  storageBucket: "wuroud-8fa99.appspot.com",
+  storageBucket: "wuroud-8fa99.firebasestorage.app",
   messagingSenderId: "1056456782887",
   appId: "1:1056456782887:web:7298bdbe27031fdeeb4a02"
 };
